16ed8f758e7f6fe3c656d746a31d0244d036f35b
Chun-Wen <hsueh.ch@northeastern.edu>
2025-01-21 16:38:55 -0800

    removed trailing spaces and add comments for class attributes

--- src/main/java/student/Greeting.java
+++ src/main/java/student/Greeting.java
@@ -4,10 +4,19 @@ package student;
  * The greeting class stores the locality ID, locality name, and a string format of the greeting both ascii and unicode.
  */
 public class Greeting {
+    /** int value of locality. Mutable */
     private int localityID;
+
+    /** name of the locality */
     private String localityName;
+
+    /** greeting string in ascii code */
     private String asciiGreeting;
+
+    /** greeting string in unicode */
     private String unicodeGreeting;
+
+    /** format string for the greeting */
     private String formatStr;
 
     /**
@@ -20,7 +29,9 @@ public class Greeting {
     }
 
     /**
-     * Greeting that creates a greeting with ascii and unicode characters assuming the language is already using ascii letters only. It also assumes the format of the greeting to be "{greeting}, {name}!"
+     * Greeting that creates a greeting with ascii and unicode characters assuming the language
+     * is already using ascii letters only. It also assumes the format of the greeting
+     * to be "{greeting}, {name}!"
      * @param localityID id of the locality
      * @param localityName name of the locality
      * @param greeting greeting using ascii characters
@@ -30,14 +41,15 @@ public class Greeting {
     }
 
     /**
-     * Greeting that creates a greeting with ascii and unicode characters. It makes no assumptions and everything needs to be provided.
+     * Greeting that creates a greeting with ascii and unicode characters.
+     * It makes no assumptions and everything needs to be provided.
      * @param localityID id of the locality
      * @param localityName name of the locality
      * @param asciiGreeting greeting using ascii characters
      * @param unicodeGreeting greeting using unicode characters
      * @param formatStr format string for the greeting
      */
-    public Greeting(int localityID, String localityName, String asciiGreeting, 
+    public Greeting(int localityID, String localityName, String asciiGreeting,
                     String unicodeGreeting, String formatStr) {
         this.localityID = localityID;
         this.localityName = localityName;
@@ -79,7 +91,8 @@ public class Greeting {
     }
 
     /**
-     * Gets the unicode format string. This string will have a %s, so that the name can be inserted into the greeting in the correct location.
+     * Gets the unicode format string. This string will have a %s,
+     * so that the name can be inserted into the greeting in the correct location.
      * @return the format string with the greeting inserted into the format
      */
     public String getFormatStr() {
@@ -87,7 +100,8 @@ public class Greeting {
     }
 
     /**
-     * Returns the format string with the greeting inserted into the format. This string will have a %s, so that the name can be inserted into the greeting in the correct location.
+     * Returns the format string with the greeting inserted into the format.
+     * This string will have a %s, so that the name can be inserted into the greeting in the correct location.
      * @param asciiOnly if true, only ascii characters will be used
      * @return the format string with the greeting inserted into the format
      */
@@ -101,7 +115,7 @@ public class Greeting {
      */
     @Override
     public String toString() {
-        return String.format("{localityID:%d, localityName:\"%s\", asciiGreeting:\"%s\", unicodeGreeting:\"%s\"}", 
+        return String.format("{localityID:%d, localityName:\"%s\", asciiGreeting:\"%s\", unicodeGreeting:\"%s\"}",
             localityID, localityName, asciiGreeting, unicodeGreeting);
     }
 }
